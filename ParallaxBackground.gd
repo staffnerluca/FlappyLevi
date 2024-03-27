123adb5b2c68a371856b9c7e45f2c518a084542a
Luca Staffner <staffnerluca@gmail.com>
2024-03-27 09:49:05 +0100

    moving enemy and player

--- ParallaxBackground.gd
+++ ParallaxBackground.gd
@@ -2,5 +2,5 @@ extends ParallaxBackground
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var scrolling_speed = 50
+	var scrolling_speed = 300
 	scroll_offset.x -= delta * scrolling_speed
---
+++ Titan.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	velocity.x = -SPEED
+	move_and_slide()
+
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		get_tree().change_scene_to_file("res://you_died_screen.tscn")
